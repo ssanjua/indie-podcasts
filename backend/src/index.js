9aee79af31ba61507fad7d0d8687cbfe7f67b74d
ssanjua <ppaupallares@gmail.com>
2024-08-22 13:20:36 +0200

    backend - improved cors policy and createPodcast

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,34 +13,20 @@ const userRoutes = require('./routes/user.js')
 const app = express()
 
 /** Middlewares */
-app.use(express.json())
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 const corsConfig = {
     credentials: true,
-    origin: true,
+    origin: process.env.CORS_ORIGIN,
 };
 
 app.use(cors(corsConfig))
-// app.use(morgan('tiny'));
-// app.disable('x-powered-by');
-// app.use(function (request, response, next) {
-//     response.header("Access-Control-Allow-Origin", "*");
-//     response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   });
 
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-// app.enable('trust proxy'); // optional, not needed for secure cookies
-// app.use(express.session({
-//     secret : '123456',
-//     key : 'sid',
-//     proxy : true, // add this when behind a reverse proxy, if you need secure cookies
-//     cookie : {
-//         secure : true,
-//         maxAge: 5184000000 // 2 months
-//     }
-// }));
+
 
 app.use("/api/auth", authRoutes)
 app.use("/api/podcasts", podcastsRoutes)
